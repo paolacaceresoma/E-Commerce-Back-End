1da816b80f29ab418888f102a4064b2c3c3bdf9d
Paola Caceres <paolacaceresoma@gmail.com>
2023-06-08 02:32:51 -0400

    started code

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//for post reqs
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//for routes
 app.use(routes);
 
-const models = require('./models');
-
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({force: false}).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-})
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
